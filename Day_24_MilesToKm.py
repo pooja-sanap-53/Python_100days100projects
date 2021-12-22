4c95f332c1a6bc1360f470e845b6b4f75f72c105
Pooja <sanappooja777@gmail.com>
2021-12-22 22:10:12 +0530

    Create Day_24_MilesToKm.py

---
+++ Day_24_MilesToKm.py
@@ -0,0 +1,37 @@
+# Day 24
+# Date: 21 Dec 2021
+# Miles to Kilometers Converter 
+
+from tkinter import *
+
+window = Tk()
+window.title("Miles to KM Converter")
+window.minsize(width=150, height=150)
+window.config(padx=20, pady=20)
+
+# Function to calculate final answer
+def ans():
+    ans = round(float(miles_entry.get()) * 1.60934, 2)
+    km.config(text=f"{ans}")
+
+# Labels
+miles = Label(text="Miles")
+miles.grid(column = 2, row=0)
+km = Label(text="Km")
+km.grid(column = 2, row=1)
+equal = Label(text="is equal to")
+equal.grid(column = 0, row=1)
+
+km = Label(text= 0, width=15)
+km.grid(column=1, row=1)
+
+# Enteries
+miles_entry = Entry(width=15)
+miles_entry.grid(column=1, row=0)
+
+
+# Button
+cal = Button(text="Calculate", command=ans)
+cal.grid(column = 1, row = 2)
+
+window.mainloop()
